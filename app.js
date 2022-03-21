602912b92a091fe399645a5cacb90700e77602d9
Will Duncan <willduncan@MBP-8323.attlocal.net>
2022-03-21 16:09:57 -0400

    create HMTL

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
+
+
+// console.log(profileDataArgs);
 
 // Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//       console.log(profileDataArr[i]);
+//     }
   
-    console.log('================');
+//     console.log('================');
   
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//     // Is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
-  printProfileData(profileDataArgs);
+//   printProfileData(profileDataArgs);
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
