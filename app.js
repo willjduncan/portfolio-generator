455e99a724c8b93dbe034b4679885a6058577f3c
Will Duncan <willduncan@MBP-8323.attlocal.net>
2022-03-23 11:05:12 -0400

    connect to node inquirer

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
 
 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
+//USED FOR LATER
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw err;
+    
+    //   console.log('Portfolio complete! Check out index.html to see the output!');
+    // });
+    
+    
+    
+    
+    
+    //NO LONGER IN USE
+// const profileDataArgs = process.argv.slice(2);
+// const [name, github] = profileDataArgs;
 // console.log(profileDataArgs);
 
 // Notice the lack of parentheses around the `profileDataArr` parameter?
@@ -20,9 +46,3 @@ const [name, github] = profileDataArgs;
 // };
 
 //   printProfileData(profileDataArgs);
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
